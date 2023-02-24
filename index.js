265616a94cb0fc6bb62526c286dc05d93dcb0cd3
Jar'Zeno <jarzenojarrett@gmail.com>
2023-02-24 09:59:33 -0800

    Rename contact-me to contact

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs')
+const url = require('url');
+
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    const q = url.parse(req.url, true);
+    console.log(q);
+    let filename = "." + q.pathname;
+    fs.readFile(filename, (err, data) => {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', "text/html");
+        res.write(data);
+        return res.end();
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server running at localhost:${port}`);
+})
